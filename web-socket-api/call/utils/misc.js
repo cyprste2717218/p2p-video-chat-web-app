126fd83ac9a9be4c71a9adcae104dc6876036d78
Stephen Ingham <smi505@york.ac.uk>
2026-05-20 00:48:40 +0100

    chore: code cleanup in ws-server.js, alongside refactoring to move websocket connection event handler logic into misc.js for better separation

---
+++ web-socket-api/call/utils/misc.js
@@ -0,0 +1,86 @@
+let wss;
+exports.wss = wss;
+
+exports.broadcast = (message) => {
+	wss.clients.forEach((client) => {
+		// Check if the connection is fully open
+		if (client.readyState === 1) {
+			client.send(JSON.stringify(message));
+		}
+	});
+}
+
+exports.sendMessageToParticipant = (targetParticipant, message) => {
+	try {
+		const participant = Array.from(wss.clients).find(client => client.username === targetParticipant);
+
+		if (!(participant && participant.readyState === 1)) {
+			throw new Error("Unable to find participant or participant ws connection not open");
+		}
+		participant.send(JSON.stringify(message));
+
+	} catch (err) {
+		console.error("Error occurred sending message to websocket client:", err);
+	}
+
+
+}
+
+exports.handleNewCallParticipantMsg = (data) => {
+
+	const { username, callId } = data;
+	console.log(`User ${username} connected to WebSocket server`);
+	const newParticipantNotif =
+	{
+		type: 'receivedNewParticipantNotif',
+		data: { message: `${username} joined chat` }
+	}
+
+
+	broadcast(newParticipantNotif);
+
+	// Returning names of current call participants to new participant to establish connections
+	const joinedCall = activeSessions.get(callId);
+	console.log("the other participants on the call:", joinedCall);
+	const { participants, pendingParticipants } = joinedCall;
+
+	const totalParticipants = participants.length + pendingParticipants.length;
+	if (totalParticipants !== 1) {
+
+		// filter out username from list of participants to connect to 
+		const otherCallParticipants = participants.filter((participant) => participant !== username);
+
+
+		const currentCallParticipantsMsg = JSON.stringify(
+			{
+				type: 'responseCurrentCallParticipants',
+				data: { participants: otherCallParticipants }
+			}
+		)
+
+
+
+		// TODO: handle update in-memory config of pending and current call participants
+		/* pendingParticipants.remove(username)
+		participants.push(username) */
+
+		return currentCallParticipantsMsg;
+	}
+}
+
+exports.handleOffer = (data) => {
+	const { caller, recipient, offer } = data;
+	console.log(`User ${caller} sent offer to ${recipient}`);
+
+	// prepare message format to return to intended recipient
+	const offerMessageToReceipient = {
+		type: 'offer',
+		data: {
+			caller: caller,
+			recipient: recipient,
+			offer: offer
+		}
+	}
+
+	sendMessageToParticipant(recipient, offerMessageToReceipient);
+}	
--- web-socket-api/call/utils/ws-server.js
+++ web-socket-api/call/utils/ws-server.js
@@ -1,39 +1,12 @@
 //require our websocket library 
 var WebSocketServer = require('ws').Server;
 const uuid = require('uuid');
-let wss;
-
-function broadcast(message) {
-	wss.clients.forEach((client) => {
-		// Check if the connection is fully open
-		if (client.readyState === 1) {
-			client.send(message);
-		}
-	});
-}
-
-function sendMessageToParticipant(targetParticipant, message) {
-	try {
-		const participant = Array.from(wss.clients).find(client => client.username === targetParticipant);
-
-		if (!(participant && participant.readyState === 1)) {
-			throw new Error("Unable to find participant or participant ws connection not open");
-		}
-		participant.send(JSON.stringify(message));
-
-	} catch (err) {
-		console.error("Error occurred sending message to websocket client:", err);
-	}
-
-
-}
+const { wss, handleOffer, handleNewCallParticipantMsg, broadcast } = require('./misc');
 
 const activeSessions = new Map();
 exports.activeSessions = activeSessions;
 
 exports.createWebSocketsServer = async () => {
-
-
 
 	function setRandomPort() {
 		function generateRandomPort() {
@@ -91,86 +64,26 @@ exports.createWebSocketsServer = async () => {
 				switch (type) {
 					case 'newParticipantOnCall':
 
-						const { username, callId } = data;
 						// setting new username property on connection (websocket client) object directly for targeting specific messages
-						connection.username = username;
+						connection.username = data.username;
 
-
-						console.log(`User ${username} connected to WebSocket server`);
-						const newParticipantNotif = JSON.stringify(
-							{
-								type: 'receivedNewParticipantNotif',
-								data: { message: `${data.username} joined chat` }
-							}
-						)
-
-						broadcast(newParticipantNotif);
-
-						// Returning names of current call participants to new participant to establish connections
-						const joinedCall = activeSessions.get(callId);
-						console.log("the other participants on the call:", joinedCall);
-						const { participants, pendingParticipants } = joinedCall;
-
-						const totalParticipants = participants.length + pendingParticipants.length;
-						if (totalParticipants !== 1) {
-
-							// filter out username from list of participants to connect to 
-							const otherCallParticipants = participants.filter((participant) => participant !== username);
-
-
-							const currentCallParticipantsMsg = JSON.stringify(
-								{
-									type: 'responseCurrentCallParticipants',
-									data: { participants: otherCallParticipants }
-								}
-							)
-
-							connection.send(currentCallParticipantsMsg)
-
-							// TODO: handle update in-memory config of pending and current call participants
-							/* pendingParticipants.remove(username)
-							participants.push(username) */
-						}
-
+						// getting return object to sendd to client
+						const currentCallParticipantsMsg = handleNewCallParticipantMsg(data);
+						connection.send(currentCallParticipantsMsg)
 						break;
 					case 'chatMessage':
-						const newChatMessage = JSON.stringify(
-							{
-								type: 'receivedNewChatMessage',
-								data: { message: data.message }
-							}
-						)
+						const newChatMessage =
+						{
+							type: 'receivedNewChatMessage',
+							data: { message: data.message }
+						}
+
 
 						broadcast(newChatMessage);
 						break;
-					case 'requestAllParticipantNames':
-						const allParticipantsNamesArr = activeSessions.callId.participants;
-
-						const allParticipantsNames = JSON.stringify(
-							{
-								type: 'allParticipantsNames',
-								data: allParticipantsNamesArr
-							}
-						)
-						break;
 					case 'offer':
-						// TODO: Implement
-						const { caller, recipient, offer } = data;
-						console.log(`User ${caller} sent offer to ${recipient}`);
 
-						// prepare message format to return to intended recipient
-						const offerMessageToReceipient = {
-							type: 'offer',
-							data: {
-								caller: caller,
-								recipient: recipient,
-								offer: offer
-							}
-						}
-
-						sendMessageToParticipant(recipient, offerMessageToReceipient);
-
-
+						handleOffer(data);
 						break;
 					default:
 						console.log("message of unrecognised type sent:", type);
