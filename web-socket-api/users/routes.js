533eb6753fffe10956f5d51db04a58a800f87a19
Stephen Ingham <smi505@york.ac.uk>
2026-05-24 18:45:07 +0100

    chore: removed .js from auth middleware import

--- web-socket-api/users/routes.js
+++ web-socket-api/users/routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const UserController = require('./controller');
-const { check } = require('../common/middlewares/isAuthenticated.js');
+const { check } = require('../common/middlewares/isAuthenticated');
 
 router.get('/', check, UserController.getUser);
 router.get('/all', check, UserController.getAllUsers);
