61312170b37d0236fb8bbf9d7ae857dd292f62fd
Stephen Ingham <smi505@york.ac.uk>
2026-05-24 18:52:51 +0100

    fix: updated auth middleware import to use actual letter casing used to avoid parsing issues in linux machine in GCP

--- web-socket-api/users/routes.js
+++ web-socket-api/users/routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const UserController = require('./controller');
-const { check } = require('../common/middlewares/isAuthenticated');
+const { check } = require('../common/middlewares/IsAuthenticated');
 
 router.get('/', check, UserController.getUser);
 router.get('/all', check, UserController.getAllUsers);
