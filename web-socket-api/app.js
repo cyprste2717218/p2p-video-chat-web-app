80639b897a0769b4ffd686b39a65b5f5a5c1cd79
Stephen Ingham <smi505@york.ac.uk>
2026-05-30 15:55:09 +0100

    chore: created refreshToken and related to User model

--- web-socket-api/app.js
+++ web-socket-api/app.js
@@ -9,7 +9,11 @@ const User = defineUser(sequelize);
 const defineCall = require('./common/models/Call');
 const Call = defineCall(sequelize);
 
+const defineResetToken = require('./common/models/ResetToken');
+const ResetToken = defineResetToken(sequelize);
+
 Call.hasMany(User, { foreignKey: 'email', as: 'calls' });
+User.hasOne(ResetToken, { foreignKey: 'tokenHash', as: 'user' })
 
 
 const express = require('express');
--- web-socket-api/common/models/User.js
+++ web-socket-api/common/models/User.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 
 const UserModel = {
 	username: { type: DataTypes.STRING, allowNull: false, unique: true },
-	email: { type: DataTypes.STRING, allowNull: false, unique: true },
+	email: { type: DataTypes.STRING, allowNull: false, unique: true, primaryKey: true },
 	password: { type: DataTypes.STRING, allowNull: false },
 };
 
