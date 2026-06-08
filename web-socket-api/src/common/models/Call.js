4d0f848c50d75d3240961868f3972c158ce68542
Stephen Ingham <smi505@york.ac.uk>
2026-06-08 17:36:04 +0100

    chore: added activeCall field to 'Call' relation and default value of 1 to totalDurationSecs

--- web-socket-api/src/common/models/Call.js
+++ web-socket-api/src/common/models/Call.js
@@ -3,9 +3,10 @@ const { DataTypes } = require('sequelize');
 const CallModel = {
 	callID: { type: DataTypes.UUID, allowNull: false, unique: true, primaryKey: true },
 	callURL: { type: DataTypes.STRING, allowNull: false },
-	totalDurationSecs: { type: DataTypes.INTEGER, allowNull: false },
+	totalDurationSecs: { type: DataTypes.INTEGER, allowNull: false, default: 1 },
 	partcipants: { type: DataTypes.JSON, allowNull: false, references: { model: 'users', key: 'email' } },
 	pendingParticipants: { type: DataTypes.JSON, allowNull: false, references: { model: 'users', key: 'email' } },
+	activeCall: { type: DataTypes.BOOLEAN }
 };
 
 module.exports = (sequelize) => sequelize.define('call', CallModel);
