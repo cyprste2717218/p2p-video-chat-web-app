13917f71d5f686da7fcdbb8ceb34721fe0cf8806
Stephen Ingham <smi505@york.ac.uk>
2026-06-08 17:56:14 +0100

    chire: updated example.data.db to reflect updated schema changes for 'call' relation

--- web-socket-api/src/common/models/Call.js
+++ web-socket-api/src/common/models/Call.js
@@ -3,10 +3,10 @@ const { DataTypes } = require('sequelize');
 const CallModel = {
 	callID: { type: DataTypes.UUID, allowNull: false, unique: true, primaryKey: true },
 	callURL: { type: DataTypes.STRING, allowNull: false },
-	totalDurationSecs: { type: DataTypes.INTEGER, allowNull: false, default: 1 },
+	totalDurationSecs: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
 	partcipants: { type: DataTypes.JSON, allowNull: false, references: { model: 'users', key: 'email' } },
 	pendingParticipants: { type: DataTypes.JSON, allowNull: false, references: { model: 'users', key: 'email' } },
-	activeCall: { type: DataTypes.BOOLEAN }
+	activeCall: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
 };
 
 module.exports = (sequelize) => sequelize.define('call', CallModel);
