1aa1e89e9395a3ea6effd36778eed4ab450283ef
Stephen Ingham <smi505@york.ac.uk>
2026-07-04 00:29:47 +0100

    chore: added rough pulumi config for gcp sql and cloud run link

--- infra/index.ts
+++ infra/index.ts
@@ -1,10 +1,75 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
-// Create a GCP resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket("my-bucket", {
-    location: "US"
+const secret=new gcp.secretmanager.Secret("secret",{
+    secretId: "secret-1",
+    replication: {
+        auto: {},
+    },
 });
-
-// Export the DNS name of the bucket
-export const bucketName = bucket.url;
+const secret_version_data=new gcp.secretmanager.SecretVersion("secret-version-data",{
+    secret: secret.name,
+    secretData: "secret-data",
+});
+const instance=new gcp.sql.DatabaseInstance("instance",{
+    name: "cloudrun-sql",
+    region: "us-central1",
+    databaseVersion: "MYSQL_5_7",
+    settings: {
+        tier: "db-f1-micro",
+    },
+    deletionProtection: true,
+});
+const _default=new gcp.cloudrunv2.Service("default",{
+    name: "cloudrun-service",
+    location: "us-central1",
+    deletionProtection: false,
+    ingress: "INGRESS_TRAFFIC_ALL",
+    scaling: {
+        maxInstanceCount: 2,
+    },
+    template: {
+        volumes: [{
+            name: "cloudsql",
+            cloudSqlInstance: {
+                instances: [instance.connectionName],
+            },
+        }],
+        containers: [{
+            image: "us-docker.pkg.dev/cloudrun/container/hello",
+            envs: [
+                {
+                    name: "FOO",
+                    value: "bar",
+                },
+                {
+                    name: "SECRET_ENV_VAR",
+                    valueSource: {
+                        secretKeyRef: {
+                            secret: secret.secretId,
+                            version: "1",
+                        },
+                    },
+                },
+            ],
+            volumeMounts: [{
+                name: "cloudsql",
+                mountPath: "/cloudsql",
+            }],
+        }],
+    },
+    traffics: [{
+        type: "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST",
+        percent: 100,
+    }],
+},{
+    dependsOn: [secret_version_data],
+});
+const project=gcp.organizations.getProject({});
+const secret_access=new gcp.secretmanager.SecretIamMember("secret-access",{
+    secretId: secret.id,
+    role: "roles/secretmanager.secretAccessor",
+    member: project.then(project => `serviceAccount:${project.number}-compute@developer.gserviceaccount.com`),
+},{
+    dependsOn: [secret],
+});
